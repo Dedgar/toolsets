a0752f03d98216138df56d4868a5a71f7d5877b2
Code Maya <ditzyd4sh@gmail.com>
2017-08-20 22:29:41 -0700

    updating

--- go/web/nep/nep.go
+++ go/web/nep/nep.go
@@ -4,49 +4,46 @@ import (
 	"html/template"
 	"net/http"
 	"regexp"
-	"fmt"
+	"strings"
 )
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "edit")
-}
-
-var templates = template.Must(template.ParseFiles("tmpl/nep_recruit.html"))
+var templates = template.Must(template.ParseFiles("tmpl/footer.html", "tmpl/header.html", "tmpl/nep_recruit.html", "tmpl/episode_view.html", "tmpl/map.html", "tmpl/menubar.html"));
 
 func renderTemplate(w http.ResponseWriter, tmpl string) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", "nep_recruit")
+	err := templates.ExecuteTemplate(w, tmpl+".html", "some_string")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-var validPath = regexp.MustCompile("^/(edit|watch|view)/([a-zA-Z0-9]+)$")
+// epview regex used to be "/watch/?:show/:season([0-9]+)/:episode([0-9]+)"
+var validPath = regexp.MustCompile("/|^/watch/divisionrune/([0-9]+)/([0-9]+)$")
+//var validPath = regexp.MustCompile("^/(episode_view|watch)/([a-zA-Z0-9]+)$")
 
-func regHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
+func episodeHandler(w http.ResponseWriter, r *http.Request, page string) {
+	renderTemplate(w, "episode_view")
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request, page string) {
+	renderTemplate(w, "nep_recruit")
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		m := validPath.FindStringSubmatch(r.URL.Path)
 		if m == nil {
 			http.NotFound(w, r)
 			return
 		}
-		fn(w, r)
-	}
-}
-
-func mainHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "nep_recruit")
-}
-
-func staticHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fn(w, r)
+    some_string := strings.Split(r.URL.Path[1:], "/")
+    //some_string := r.URL.Path[1:]
+		fn(w, r, some_string[1])
 	}
 }
 
 func main() {
-	http.HandleFunc("/", staticHandler(mainHandler))
-	http.HandleFunc("/edit/", regHandler(editHandler))
+	http.HandleFunc("/", makeHandler(mainHandler))
+	http.HandleFunc("/watch/", makeHandler(episodeHandler))
 
-  fmt.Println("starting server")
 	http.ListenAndServe(":8080", nil)
 }
